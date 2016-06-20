27b6132901574598e313b119f3e0a3ea0d6b9beb
Daniel Crussi and Stephen Snow <pair+dcrut+ssnow@allstate.com>
2016-06-20 17:02:56 -0500

    whateva

--- tipcalc.js
+++ tipcalc.js
@@ -11,3 +11,15 @@ app.use(bodyParser.urlencoded({ extended: true } ));
 app.listen(3000, function(){
   console.log('Server Started...')
 });
+
+app.get('/tipcalc', function(req, res){
+  const tips = ['5', '10', '15', '20'];
+
+  res.render('calc', {tips, result: 0});
+});
+
+app.post('/tipcalc', function(req, res){
+  tip = req.body.tip;
+
+  res.render('calc', {tips, result: 0});
+});
